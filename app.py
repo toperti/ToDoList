9f6e9a2522623b558c04a0219cca660c8b16056b
Edwin G <edwgeo@gmail.com>
2020-01-03 19:17:00 -0800

    Update Functionality added

--- app.py
+++ app.py
@@ -47,5 +47,21 @@ def delete(id):
 		return "There was an error deleting that task"
 
 
+@app.route('/update/<int:id>', methods = ['GET', 'POST'])
+def update(id):
+	task = Todo.query.get_or_404(id)
+
+	if request.method == 'POST':
+		task.content = request.form['content']
+
+		try:
+			db.session.commit()
+			return redirect('/')
+			
+		except:
+			return "There was an error with updating the task"	
+	else:
+		return render_template('update.html', task = task)
+
 if __name__ == '__main__':
 	app.run(debug = True)
